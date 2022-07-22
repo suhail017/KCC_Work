c8a54d7aec2920c8ec0b92b0530a78a304827fb5
Suhail <suhail017@gmail.com>
2022-07-22 12:50:47 -0400

    Adding the Downscaling function

--- convert_png_rgb.py
+++ convert_png_rgb.py
@@ -27,3 +27,18 @@ for filename in os.listdir(path):
     im.save(f"{path}\{filename.split('.')[0]}.jpg") 
     
     
+#%% Downscale the file Size
+
+from PIL import Image
+import os
+import PIL
+import glob
+input_path = r'C:\Users\smahmud\Desktop\MCS Dataset\images\jpg\NonDerecho'
+#output_path = r'C:\Users\smahmud\Desktop\MCS Dataset\images\resize_NonDerecho'
+images = [file for file in os.listdir(input_path) if file.endswith(('jpeg', 'png', 'jpg'))]
+
+for image in images:
+    img = Image.open(os.path.join(input_path,image))
+   
+    img.thumbnail((600,600))
+    img.save(r'C:\Users\smahmud\Desktop\MCS Dataset\images\resize_NonDerecho\  resized_'+image, optimize=True, quality=40)
